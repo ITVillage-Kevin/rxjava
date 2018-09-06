848ae9f3ad786e48c1ab37ca9eca3efb71dd65a1
ITVillage-Kevin <it.village.host@gmail.com>
2018-09-06 10:07:39 +0900

    Add Hello world sample code to chapter02

---
+++ src/main/java/com/itvillage/chapter01/ImperativeProgramming.java
@@ -0,0 +1,20 @@
+package com.itvillage.chapter01;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ImperativeProgramming {
+    public static void main(String[] args){
+        // List에 있는 숫자들 중에서 6보다 큰 홀수들의 합계를 구하세요.
+        List<Integer> numbers = Arrays.asList(1, 3, 21, 10, 8, 11);
+        int sum = 0;
+
+        for(int number : numbers){
+             if(number > 6 && (number % 2 != 0)){
+                 sum += number;
+             }
+        }
+
+        System.out.println("# 명령형 프로그래밍 사용: " + sum);
+    }
+}
---
+++ src/main/java/com/itvillage/chapter01/ToDoSample.java
@@ -0,0 +1,21 @@
+package com.itvillage.chapter01;
+
+import io.reactivex.Observable;
+import io.reactivex.schedulers.Schedulers;
+
+public class ToDoSample {
+    public static void main(String[] args) throws InterruptedException {
+        Observable.just(100, 200, 300, 400, 500)
+                .doOnNext(data -> System.out.println(getThreadName() + " : " + data))
+                .subscribeOn(Schedulers.io())
+                .observeOn(Schedulers.computation())
+                .filter(number -> number > 300)
+                .subscribe(num -> System.out.println(getThreadName() + " : result : " + num));
+
+        Thread.sleep(500);
+    }
+
+    public static String getThreadName(){
+        return Thread.currentThread().getName();
+    }
+}
