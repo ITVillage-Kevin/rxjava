0482b099d115db9cad6dd003cb0cf60df6f56f18
ITVillage-Kevin <it.village.host@gmail.com>
2020-02-06 09:43:55 +0900

    .

---
+++ src/main/java/com/itvillage/chapter04/transform/MapExample.java
@@ -0,0 +1,19 @@
+package com.itvillage.chapter04.transform;
+
+import com.itvillage.utils.Logger;
+import io.reactivex.Observable;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Observable이 배출한 항목에 함수를 적용하여 배출된 값을 변환시킨다.
+ */
+public class MapExample {
+    public static void main(String[] args){
+        List<Integer> oddList = Arrays.asList(1, 3, 5, 7);
+        Observable.fromIterable(oddList)
+                .map(num -> num + 1)
+                .subscribe(Logger::on);
+    }
+}
