4880bebf92b0a7735ed99f095cd30263dab0e092
ITVillage-Kevin <it.village.host@gmail.com>
2020-02-13 09:37:08 +0900

    Modify ObservableDeferExample.java

--- src/main/java/com/itvillage/chapter05/chapter0501/ObservableDeferExample.java
+++ src/main/java/com/itvillage/chapter05/chapter0501/ObservableDeferExample.java
@@ -3,19 +3,27 @@ package com.itvillage.chapter05.chapter0501;
 import com.itvillage.utils.Logger;
 import io.reactivex.Observable;
 
+import java.time.LocalTime;
+
+/**
+ * 실제 구독이 발생할 때 Observable을 새로 반환하여 새로운 Observable을 생성한다.
+ * defer()를 활용하면 데이터 흐름의 생성을 지연하는 효과를 보여준다.
+ */
 public class ObservableDeferExample {
     public static void main(String[] args) throws InterruptedException {
-        // 실제 구독이 발생할 때 Observable을 새로 반환하여 새로운 Observable을 생성한다.
-        // defer()를 활용하면 데이터의 흐름의 생성을 지연하는 효과를 보여준다.
-        Observable<Long> observable = Observable.defer(() -> {
-            long time = System.currentTimeMillis();
-            return Observable.just(time);
+        Observable<LocalTime> observable = Observable.defer(() -> {
+            LocalTime currentTime = LocalTime.now();
+            return Observable.just(currentTime);
         });
 
-        observable.subscribe(time -> Logger.print(" # 구독자 1의 구독 시간: " + time));
+        Observable<LocalTime> observableJust = Observable.just(LocalTime.now());
+
+        observable.subscribe(time -> Logger.print(" # defer() 구독자 1의 구독 시간: " + time));
+        observableJust.subscribe(time -> Logger.print(" # just() 구독자 1의 구독 시간: " + time));
 
         Thread.sleep(3000);
 
-        observable.subscribe(time -> Logger.print(" # 구독자 1의 구독 시간: " + time));
+        observable.subscribe(time -> Logger.print(" # defer() 구독자 1의 구독 시간: " + time));
+        observableJust.subscribe(time -> Logger.print(" # just() 구독자 1의 구독 시간: " + time));
     }
 }
