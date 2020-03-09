003afead7c095c0ac39b58bfe12dd24a77113027
ITVillage-Kevin <it.village.host@gmail.com>
2020-03-10 07:55:53 +0900

    Complete BlockingGetTest, AssertEmptyTest examples

--- src/test/java/com/itvillage/chapter09/chapter0901/BlockingGetTest.java
+++ src/test/java/com/itvillage/chapter09/chapter0901/BlockingGetTest.java
@@ -1,6 +1,8 @@
 package com.itvillage.chapter09.chapter0901;
 
+import io.reactivex.Maybe;
 import io.reactivex.Observable;
+import io.reactivex.Single;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -14,12 +16,12 @@ public class BlockingGetTest extends RxJavaTest {
     @Test
     public void totalSalesOfBranchATest(){
         // when
-        int totalSales = sampleObservable.getSalesOfBranchA()
-                .reduce((a, b) -> a + b)
-                .blockingGet();
+//        int totalSales = sampleObservable.getSalesOfBranchA()
+//                .reduce((a, b) -> a + b)
+//                .blockingGet();
 
         // then
-        assertThat(totalSales, is(326_000_000));
+        assertThat(Observable.empty().firstElement().blockingGet(), is(326_000_000));
     }
 
     // A, B, C 지점의 연간 매출 합계 테스트
